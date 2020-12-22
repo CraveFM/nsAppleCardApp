8521f2c9d0ddf055d7b39ded95294bc5638e8494
SetraR <setrar@valiha.com>
2020-12-21 19:43:52 -0500

    .

---
+++ src/app/apple-card/apple-card-animation/apple-card-animation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ns-apple-card-animation',
+  templateUrl: './apple-card-animation.component.html',
+  styleUrls: ['./apple-card-animation.component.scss']
+})
+export class AppleCardAnimationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/apple-card/apple-card.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppleCardRoutingModule } from './apple-card-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AppleCardRoutingModule
+  ]
+})
+export class AppleCardModule { }
